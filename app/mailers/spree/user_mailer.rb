3fc15d852c048d2cfd838931d996b85688efeaf7
fxgit <freddy.borbor@gmail.com>
2016-04-14 17:26:16 -0500

    fixed wrong variable name

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,7 +1,7 @@
 module Spree
   class UserMailer < BaseMailer
     def welcome_email(user)
-      @user = user.respond_to?(:id) ? order : Spree::User.find(user)
+      @user = user.respond_to?(:id) ? user : Spree::User.find(user)
       subject = "Welcome #{@user.email}"
       mail(to: @user.email, from: from_address, subject: subject)
     end
