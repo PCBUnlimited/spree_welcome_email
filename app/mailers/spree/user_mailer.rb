b1c4ef6b03d036a35756efb20b98dba0b4a62404
fxgit <freddy.borbor@gmail.com>
2016-04-15 15:06:16 -0500

    allow override email subject

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -2,8 +2,13 @@ module Spree
   class UserMailer < BaseMailer
     def welcome_email(user)
       @user = user.respond_to?(:id) ? user : Spree::User.find(user)
-      subject = "Welcome #{@user.email}"
+      subject = welcome_email_subject(@user)
       mail(to: @user.email, from: from_address, subject: subject)
     end
+
+    private
+      def welcome_email_subject(user)
+        "Welcome #{user.email}"
+      end
   end
 end
